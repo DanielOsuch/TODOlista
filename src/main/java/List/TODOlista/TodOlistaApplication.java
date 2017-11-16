83e6cb707de3bc7e34c338603b8a7f2d219b8036
Daniel Osuch <dunio92@outlook.com>
2017-11-16 16:38:54 +0100

    first commit

---
+++ src/main/java/List/TODOlista/TodOlistaApplication.java
@@ -0,0 +1,12 @@
+package List.TODOlista;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodOlistaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodOlistaApplication.class, args);
+	}
+}
