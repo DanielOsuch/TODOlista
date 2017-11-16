4555e95a56f09f61e5a604c1d84b99497f5d2339
Daniel Osuch <dunio92@outlook.com>
2017-11-16 16:57:51 +0100

    added homeController

--- src/main/java/List/TODOlista/controller/HomeController.java
+++ src/main/java/List/TODOlista/controller/HomeController.java
@@ -1,4 +1,23 @@
 package List.TODOlista.controller;
 
+import List.TODOlista.model.ToDo;
+import List.TODOlista.repository.ToDoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
 public class HomeController {
+
+    @Autowired
+    private ToDoRepository toDoRepository;
+
+    @GetMapping("/")
+    public @ResponseBody String addToDoList(@RequestParam String name,@RequestParam String todo,@RequestParam String status){
+        ToDo toDo = new ToDo();
+
+        return "home";
+    }
 }
